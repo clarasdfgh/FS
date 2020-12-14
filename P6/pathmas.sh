552a168203299d766c7194bc30679eb5f451804e
clarasdfgh <claramariaromerolara@gmail.com>
2020-12-14 14:35:40 +0100

    Commit temeroso de dios

---
+++ P6/pathmas.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+# Uso: pathmas directorio [after] 
+if ! echo $PATH | /bin/egrep -q "(^|:)$1($|:)" ; then 
+		if [ "$2" == "after" ] ; then # <<<<<<<<<<<<<<<<<<<<< 
+				PATH=$PATH:$1 
+			else 
+				PATH=$1:$PATH 
+		fi 
+	else echo "$1 ya está en el path" 
+fi
